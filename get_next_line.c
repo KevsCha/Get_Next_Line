a2ff27895191979a564e88c62ca576d058d40b86
KevsCha <k-e-v-i-n_13_@hotmail.com>
2023-12-13 16:13:30 +0100

    ultimos cambios

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kquispe <kquispe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 18:10:57 by kquispe           #+#    #+#             */
-/*   Updated: 2023/12/13 15:29:25 by kquispe          ###   ########.fr       */
+/*   Updated: 2023/12/13 16:10:41 by kquispe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,14 @@ static char	*ft_buff_change(char *buff)
 			j++;
 	aux = ft_calloc(j + 1, sizeof(char));
 	if (!aux)
-		return (free(buff), NULL);
+		return (free(buff), buff = NULL, NULL);
 	while (k < j || k < 0)
 	{
 		aux[k] = buff[i + 1 + k];
 		k++;
 	}
 	free(buff);
+	buff = NULL;
 	return (aux);
 }
 
@@ -53,7 +54,7 @@ static char	*ft_return_line(char *buff)
 		i++;
 	temp = ft_calloc((size_t)i + 1, sizeof(char));
 	if (!temp)
-		return (free(buff), NULL);
+		return (free(buff), buff = NULL, NULL);
 	while (buff[j] && buff[j] != '\n')
 	{
 		temp[j] = buff[j];
@@ -72,20 +73,19 @@ static char	*ft_line_found(char *buff, int fd)
 	txt = 1;
 	temp = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	if (!temp)
-		return (free(buff), NULL);
+		return (free(buff), buff = NULL, NULL);
 	while (txt != 0 && !ft_strchr(buff, '\n'))
 	{
 		txt = read(fd, temp, BUFFER_SIZE);
 		if (txt == 0)
-			return (free(temp), buff);
+			return (free(temp), temp = NULL, buff);
 		if (txt == -1)
-			return (free(temp), NULL);
+			return (free(temp), temp = NULL, NULL);
 		buff = ft_strjoin(buff, temp);
 		if (!buff)
-			return (free(temp), buff = NULL, NULL);
+			return (free(temp), temp = NULL, NULL);
 	}
-	free(temp);
-	return (buff);
+	return (free(temp), temp = NULL, buff);
 }
 
 char	*get_next_line(int fd)
@@ -99,16 +99,16 @@ char	*get_next_line(int fd)
 		return (free(buff), buff = NULL, NULL);
 	buff = ft_line_found(buff, fd);
 	if (!buff)
-		return (free(buff), NULL);
+		return (free(buff), buff = NULL, NULL);
 	while (buff[i])
 		i++;
 	if (i == 0)
-		return (NULL);
+		return (free(buff), buff = NULL, NULL);
 	temp = ft_return_line(buff);
 	if (!temp)
-		return (free(buff), NULL);
+		return (free(buff), buff = NULL, NULL);
 	buff = ft_buff_change(buff);
 	if (!buff)
-		return (NULL);
+		return (free(temp), temp = NULL, NULL);
 	return (temp);
 }
